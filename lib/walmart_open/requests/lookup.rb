f813bcf8b175984826b6c6dbe0af12fcd9553a2e
Jedford Seculles <jfjade18@gmail.com>
2015-02-03 21:46:56 +0800

    allows to lookup for multiple item ids, supports up to 20 items in one call

--- lib/walmart_open/requests/lookup.rb
+++ lib/walmart_open/requests/lookup.rb
@@ -6,14 +6,20 @@ module WalmartOpen
   module Requests
     class Lookup < Request
       def initialize(item_id, params = {})
-        self.path = "items/#{item_id}"
+        self.path = 'items' + (item_id.to_s.empty? ? '' : "/#{item_id}")
+        # accepts multiple ids, comma-separated string
+        # {ids: "12345678,23456789,34567890"}
         self.params = params
       end
 
       private
 
       def parse_response(response)
-        Item.new(response.parsed_response)
+        if response.parsed_response.has_key?('items')
+          response.parsed_response['items'].map {|item| Item.new(item)}
+        else
+          Item.new(response.parsed_response)
+        end
       end
 
       def verify_response(response)
